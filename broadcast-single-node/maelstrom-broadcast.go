eee9395199930a94ea3ac16249db9c6c272ad7e4
Jon Barber <jon.barber@acm.org>
2024-04-03 10:41:58 +0100

    Refactor broadcast

--- broadcast-single-node/maelstrom-broadcast.go
+++ broadcast-single-node/maelstrom-broadcast.go
@@ -7,56 +7,73 @@ import (
 	"sync"
 )
 
+type app struct {
+	mx       sync.RWMutex
+	messages []float64
+	n        *maelstrom.Node
+}
+
+func newApp(n *maelstrom.Node) *app {
+	a := &app{
+		n: n,
+	}
+
+	n.Handle("broadcast", a.broadcast)
+	n.Handle("read", a.read)
+	n.Handle("topology", a.topology)
+
+	return a
+}
+
+func (a *app) broadcast(msg maelstrom.Message) error {
+	// Unmarshal the message body as a loosely-typed map.
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	message := body["message"].(float64)
+
+	a.mx.Lock()
+	defer a.mx.Unlock()
+	a.messages = append(a.messages, message)
+
+	response := map[string]any{
+		"type": "broadcast_ok",
+	}
+
+	return a.n.Reply(msg, response)
+}
+
+func (a *app) read(msg maelstrom.Message) error {
+	a.mx.Lock()
+	defer a.mx.Unlock()
+
+	response := map[string]any{
+		"type":     "read_ok",
+		"messages": a.messages,
+	}
+
+	return a.n.Reply(msg, response)
+
+}
+
+func (a *app) topology(msg maelstrom.Message) error {
+	a.mx.Lock()
+	defer a.mx.Unlock()
+
+	response := map[string]any{
+		"type": "topology_ok",
+	}
+
+	return a.n.Reply(msg, response)
+
+}
+
 func main() {
 
-	mutex := sync.RWMutex{}
-	var messages []float64
-
 	n := maelstrom.NewNode()
-
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-
-		// Unmarshal the message body as a loosely-typed map.
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		message := body["message"].(float64)
-
-		mutex.Lock()
-		defer mutex.Unlock()
-		messages = append(messages, message)
-
-		response := map[string]any{
-			"type": "broadcast_ok",
-		}
-
-		return n.Reply(msg, response)
-
-	})
-
-	n.Handle("read", func(msg maelstrom.Message) error {
-
-		mutex.Lock()
-		defer mutex.Unlock()
-
-		response := map[string]any{
-			"type":     "read_ok",
-			"messages": messages,
-		}
-
-		return n.Reply(msg, response)
-	})
-
-	n.Handle("topology", func(msg maelstrom.Message) error {
-
-		response := map[string]any{
-			"type": "topology_ok",
-		}
-
-		return n.Reply(msg, response)
-	})
+	newApp(n)
 
 	if err := n.Run(); err != nil {
 		slog.Error("Unable to run the handler", slog.Any("error", err))
