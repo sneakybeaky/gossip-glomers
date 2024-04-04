c346c3005d5f4e33f96278203758ccdd703add00
Jon Barber <jon.barber@acm.org>
2024-04-04 18:10:41 +0100

    Refactor to remove unused context

--- broadcast-fault-tolerant/maelstrom-fault-tolerant.go
+++ broadcast-fault-tolerant/maelstrom-fault-tolerant.go
@@ -3,15 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"github.com/failsafe-go/failsafe-go"
 	"github.com/failsafe-go/failsafe-go/retrypolicy"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
-	"golang.org/x/sync/errgroup"
-	"log"
 	"log/slog"
 	"os"
-	"os/signal"
 	"sync"
 	"time"
 )
@@ -22,10 +19,9 @@ type app struct {
 	n          *maelstrom.Node
 	neighbours []string
 	logger     *slog.Logger
-	ctx        context.Context
 }
 
-func newApp(ctx context.Context, n *maelstrom.Node) *app {
+func newApp(n *maelstrom.Node) *app {
 
 	opts := &slog.HandlerOptions{
 		Level: slog.LevelDebug,
@@ -34,7 +30,6 @@ func newApp(ctx context.Context, n *maelstrom.Node) *app {
 		n:        n,
 		logger:   slog.New(slog.NewTextHandler(os.Stderr, opts)),
 		messages: make(map[float64]bool),
-		ctx:      ctx,
 	}
 
 	n.Handle("broadcast", a.handleBroadcast)
@@ -67,7 +62,7 @@ type broadcaster struct {
 	retryPolicy retrypolicy.RetryPolicy[maelstrom.Message]
 }
 
-func (b broadcaster) broadcast(ctx context.Context) error {
+func (b broadcaster) broadcast() error {
 
 	type responseBody struct {
 		Type string `json:"type"`
@@ -76,9 +71,9 @@ func (b broadcaster) broadcast(ctx context.Context) error {
 	b.logger.Debug("Sending broadcast to neighbour")
 
 	resp, err := failsafe.Get[maelstrom.Message](func() (maelstrom.Message, error) {
-		ctxt, cancel := context.WithTimeout(ctx, 10*time.Millisecond)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
 		defer cancel()
-		return b.rpc(ctxt, b.dest, b.body)
+		return b.rpc(ctx, b.dest, b.body)
 	}, b.retryPolicy)
 
 	if err != nil {
@@ -133,7 +128,7 @@ func (a *app) handleBroadcast(msg maelstrom.Message) error {
 					HandleErrors(context.DeadlineExceeded).
 					WithDelay(5 * time.Millisecond).WithMaxRetries(-1).Build(),
 			}
-			_ = b.broadcast(a.ctx)
+			_ = b.broadcast()
 		}(neighbour)
 
 	}
@@ -195,27 +190,11 @@ func (a *app) handleTopology(msg maelstrom.Message) error {
 
 func main() {
 
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
-	g, gctx := errgroup.WithContext(ctx)
-
-	g.Go(func() error {
-
-		n := maelstrom.NewNode()
-		return newApp(gctx, n).Run()
-
-	})
-
-	err := g.Wait()
+	n := maelstrom.NewNode()
+	err := newApp(n).Run()
 	if err != nil {
-		if errors.Is(err, context.Canceled) {
-			log.Println("context was canceled")
-		} else {
-			log.Printf("received error: %v\n", err)
-		}
-	} else {
-		log.Println("finished clean")
+		fmt.Fprintf(os.Stderr, "failed to run app : %+v", err)
+		os.Exit(1)
 	}
 
 }
